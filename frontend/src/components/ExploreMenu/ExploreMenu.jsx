19fbb44bd91732fa0250ac210a1e1ca6688c31ac
TARUN <111377394+tarun170104@users.noreply.github.com>
2024-08-27 19:19:20 +0530

    Add files via upload

---
+++ frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -0,0 +1,28 @@
+import React, { useContext } from 'react'
+import './ExploreMenu.css'
+import { StoreContext } from '../../Context/StoreContext'
+
+const ExploreMenu = ({category,setCategory}) => {
+
+  const {menu_list} = useContext(StoreContext);
+  
+  return (
+    <div className='explore-menu' id='explore-menu'>
+      <h1>Explore our menu</h1>
+      <p className='explore-menu-text'>Savor the flavors of the world, delivered right to your doorstep, Choose your Dishes</p>
+      <div className="explore-menu-list">
+        {menu_list.map((item,index)=>{
+            return (
+                <div onClick={()=>setCategory(prev=>prev===item.menu_name?"All":item.menu_name)} key={index} className='explore-menu-list-item'>
+                    <img src={item.menu_image} className={category===item.menu_name?"active":""} alt="" />
+                    <p>{item.menu_name}</p>
+                </div>
+            )
+        })}
+      </div>
+      <hr />
+    </div>
+  )
+}
+
+export default ExploreMenu
---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <div className='header-contents'>
+                <h2>Choose Your Favourite Food</h2>
+                <p>Food that travels well, straight to your door because convenience never tasted this good</p>
+            </div>
+        </div>
+    )
+}
+
+export default Header
